07476176d8c77b081511e9f5c6eb3821a5dc9f21
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-04 17:13:27 +0800

    update file for exercise 3. Completed exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let textLength = text.length;
+    let textArray = text.split("");
+    let textArrayReverse = textArray.reverse();
+
+    let textReverse = '';
+
+    for (const letter of textArrayReverse) {
+        textReverse = textReverse + letter;
+    };
+
+    return textReverse;
 
 };
 
