bd0cc46d114d5daecc77d742f0b3d95c60cc1a3e
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-04 21:28:47 +0800

    updated file for second task on excluding multiple value from array. Completed second task.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,21 @@
-const removeFromArray = function(array, removeItem) {
+const removeFromArray = function() {
+    //take all arguments and put them in array. the first element is array.
+    const args = Array.from(arguments) 
+
+    //get the array in the first element
+    const array = args.slice(0,1);
+
+    //get the exclude item as array
+    const removeArray = args.slice(1)
+    
     newArray = [];
 
-    for (const item of array) {
-        if (item !== removeItem) {
+    for (const item of array[0]) {
+
+        //test if item is to be included
+        let excludeItem = removeArray.includes(item);
+
+        if (excludeItem == false) {
             newArray.push(item);
         } else {
             continue
