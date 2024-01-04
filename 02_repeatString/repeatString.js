06be92af267092d3773d7b8e31dc663337a99c0c
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-04 16:38:24 +0800

    Updated exercise file. Completed exercise #2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(text,n_repeat) {
+    let textConcat = '';
 
+    if (n_repeat < 0) {return 'ERROR'};
+    
+    for (let i = 0; i < n_repeat; i++) {
+        textConcat = textConcat + text;
+    };
+
+    return textConcat
 };
 
 // Do not edit below this line
