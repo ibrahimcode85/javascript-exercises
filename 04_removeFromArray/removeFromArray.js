bcbb53003d3d869d83d7b1f3ebaeb4a6160cdf06
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-04 20:58:46 +0800

    add new file for exercise #4. completed the first task of this exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeItem) {
+    newArray = [];
 
+    for (const item of array) {
+        if (item !== removeItem) {
+            newArray.push(item);
+        } else {
+            continue
+        };
+    };
+
+    return newArray;
 };
 
 // Do not edit below this line
