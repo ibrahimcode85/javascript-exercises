56d23891f66a06609bfd6b1dadb1946df2f65348
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-08 16:27:16 +0800

    Modified files for exercise #6. Completed exercise #6.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,28 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    //define initial state
+    let div4 = false;
+    let div100 = false;
+    let div400 = false;
+
+    if (year % 4   === 0) {div4   = true};
+    if (year % 100 === 0) {div100 = true};
+    if (year % 400 === 0) {div400 = true};
+
+
+    if (div4){
+        if (div100){
+            if (div400){
+                return true;
+            }else{
+                return false;
+            };
+        }else{
+            return true;
+        };
+    }else{
+        return false;
+    };
 };
 
 // Do not edit below this line
