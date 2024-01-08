2b7cc3b1bc33674fef559196081fdcd258fbb3a5
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-08 15:57:15 +0800

    Modified files for exercise #5. Completed exercise #5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    
+    //only number datatype
+    if (typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR';
+    }
+    
+    //only positive number
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    };
 
+    if (start > end){
+        startSum = end;
+        endSum = start;
+
+    } else {
+        startSum = start;
+        endSum  = end;
+    }
+
+    sumNum = 0;
+
+    for (let num = startSum; num <= endSum; num++ ) {
+        sumNum = sumNum + num;
+    };
+
+    return sumNum;
 };
 
 // Do not edit below this line
