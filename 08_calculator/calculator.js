94a1553d33635ba1f2d9f5c28b4667f327f3c6fe
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-29 16:14:09 +0800

    Modified files for exercise #8. Completed exercise #8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,65 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function([...numbers]) {
+	if (numbers.length === 0){
+    return 0;
+
+  }else if (numbers.length === 1) {
+    return numbers[0];
+
+  }else {
+    let numTotal = 0;
+
+    for (let num of numbers) {
+      numTotal = numTotal + num;
+    };
+
+    return numTotal;
+
+  };
+
 };
 
-const multiply = function() {
+const multiply = function([...numbers]) {
+  if (numbers.length === 0){
+    return 0;
 
+  }else if (numbers.length === 1) {
+    return numbers[0];
+
+  }else {
+    let numMultiply = 1;
+
+    for (let num of numbers) {
+      numMultiply = numMultiply * num;
+    };
+
+    return numMultiply;
+    
+  };
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1;
+  }else {
+    numFactorial = 1;
+
+    for (let num = 1; num <= number; num++){
+      numFactorial = numFactorial * num;
+    };
+    return numFactorial;
+  };
 };
 
 // Do not edit below this line
