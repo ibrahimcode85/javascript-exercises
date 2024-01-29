cf6b538698afc77da1700007ab01e716a625e569
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-29 17:01:36 +0800

    Update files for exercise #9. Completed exercise #9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let punctuation = /[\.,?!]/g;
+    let spaces = / /g;
+    let wordReverse = '';
+
+    //cleanup the string to be checked
+    wordClean = word.replace(punctuation, '');    // remove punctuation
+    wordClean = wordClean.toLowerCase();           // all lower case
+    wordClean = wordClean.replace(spaces, '');     // remove spaces
+
+    //construct string in reverse call order
+    for (let i= wordClean.length - 1; i >= 0; i--){
+        wordReverse = wordReverse + wordClean[i];
+    };
+
+    //return true if match with original word
+    if (wordReverse === wordClean){
+        return true;
+    }else{
+        return false;
+    };
 
 };
 
