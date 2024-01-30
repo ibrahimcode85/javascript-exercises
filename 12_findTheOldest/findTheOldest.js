ae3d0c66cb687cf131ae710de1425eec3fb7b169
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-30 16:15:29 +0800

    Update files for exercise #12. Completed exercise #12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    // calculate age of each person
+    let arrayWithAge = array.map((people) => {
 
+        if (people.yearOfDeath){
+            age = people.yearOfDeath - people.yearOfBirth;
+        }else{
+            age = (new Date()).getFullYear() - people.yearOfBirth;
+        };
+    
+        return {'name': people.name, 'age': age};
+    });
+
+    // sort by the oldest
+    let ageSorted = arrayWithAge.sort((firstPerson, lastPerson) => {
+        if(firstPerson.age < lastPerson.age){
+            return 1;
+        }else{
+            return -1;
+        };
+    });
+
+    // return the first array
+    return ageSorted[0];
 };
 
 // Do not edit below this line
